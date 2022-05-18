a2de7c78f21a9b0fd76efd5b844503a49263209d
Pavel Egorov <egorovpvspb@gmail.com>
2022-05-18 14:58:20 +0300

    Release 0. Итерационный подсчет факториала

---
+++ index.js
@@ -0,0 +1,11 @@
+
+function iterativeFactorialCalculation(num) {
+  let result = 1;
+  for(let i = 1; i <= num; i++) {
+    result*=i;
+  }
+  return result;
+}
+console.log(iterativeFactorialCalculation(5)) 
+
+
